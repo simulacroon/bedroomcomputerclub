398e901d02300aca56771903ee12ba0462a606cb
simulacroon <167260610+simulacroon@users.noreply.github.com>
2026-03-30 15:53:15 -0500

    Update simu.js

--- simu.js
+++ simu.js
@@ -112,3 +112,19 @@ const circleObserver = new IntersectionObserver((entries)=>{
 
 circleVideos.forEach(v => circleObserver.observe(v));
 
+
+<script>
+const observer = new IntersectionObserver((entries)=>{
+  entries.forEach(entry=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("visible");
+    }
+  });
+},{
+  threshold:0.2
+});
+
+document.querySelectorAll(".reveal").forEach(el=>{
+  observer.observe(el);
+});
+</script>
